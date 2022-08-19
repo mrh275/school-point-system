f0339bda774212f8d4b143eed307da2fac1d73c3
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-19 11:19:34 +0700

    added router view

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <navbar></navbar>
+  <router-view></router-view>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
