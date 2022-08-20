3bc2f4990f3505d5316e905ba2a20ec9d2c470da
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-20 23:09:00 +0700

    added notification icon

--- src/components/partials/UserGreetings.vue
+++ src/components/partials/UserGreetings.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="user-wrapper flex items-center">
-    <img :src="userImage" alt="User Profile" class="w-14 rounded-full" />
-    <div class="user-detail mx-4">
-      <h4 class="user-name font-bold text-xl">Halo, John Doe</h4>
-      <span class="text-sm text-gray-500">Administrator</span>
+  <div class="user-wrapper flex items-center justify-between">
+    <div class="user-greet flex items-center">
+      <img :src="userImage" alt="User Profile" class="w-14 rounded-full" />
+      <div class="user-detail mx-4">
+        <h4 class="user-name font-bold text-xl">Halo, John Doe</h4>
+        <span class="text-sm text-gray-500">Administrator</span>
+      </div>
+    </div>
+    <div class="notif-btn cursor-pointer relative">
+      <i class="bx bxs-bell bx-md text-gray-500"></i>
+      <span class="notif-badge bg-red-500 right-1 leading-[.55] text-white pt-[5px] px-[3px] rounded-full absolute">*</span>
     </div>
   </div>
 </template>
