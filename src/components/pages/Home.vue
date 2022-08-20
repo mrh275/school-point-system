c14b177fd0b99c1776fb230a940e40eac3034b9a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-20 22:52:30 +0700

    change font weight

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,7 +2,7 @@
   <div class="user-wrapper flex items-center">
     <img :src="userImage" alt="User Profile" class="w-14 rounded-full" />
     <div class="user-detail mx-4">
-      <h4 class="user-name font-semibold text-xl">Halo, John Doe</h4>
+      <h4 class="user-name font-bold text-xl">Halo, John Doe</h4>
       <span class="text-sm text-gray-500">Administrator</span>
     </div>
   </div>
