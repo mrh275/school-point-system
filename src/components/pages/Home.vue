7f7b51be542986767db73b1eb873fdfd268d34e1
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-20 22:57:16 +0700

    Saparate user greeting section

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="user-wrapper flex items-center">
-    <img :src="userImage" alt="User Profile" class="w-14 rounded-full" />
-    <div class="user-detail mx-4">
-      <h4 class="user-name font-bold text-xl">Halo, John Doe</h4>
-      <span class="text-sm text-gray-500">Administrator</span>
-    </div>
-  </div>
+  <user-greet></user-greet>
 </template>
 
 <script>
-import userImg from "../../assets/img/user-square.png";
+import UserGreetings from "../partials/UserGreetings.vue";
 export default {
   name: "Home",
-  data() {
-    return {
-      userImage: userImg,
-    };
+  components: {
+    UserGreet: UserGreetings,
   },
   mounted() {
     document.title = "Beranda";
---
+++ src/components/partials/UserGreetings.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="user-wrapper flex items-center">
+    <img :src="userImage" alt="User Profile" class="w-14 rounded-full" />
+    <div class="user-detail mx-4">
+      <h4 class="user-name font-bold text-xl">Halo, John Doe</h4>
+      <span class="text-sm text-gray-500">Administrator</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import userImg from "../../assets/img/user-square.png";
+export default {
+  data() {
+    return {
+      userImage: userImg,
+    };
+  },
+};
+</script>
