3df94e498ccf7e74cfc1374024d530c797d758f4
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-20 11:10:55 +0700

    added transition duration

---
+++ src/components/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-3xl font-bold">Home Page</h1>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  mounted() {
+    document.title = "Beranda";
+  },
+};
+</script>
