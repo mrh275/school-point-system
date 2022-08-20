34648194952cb4388831e4a2997fd3122a4db517
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-20 23:40:49 +0700

    create login page

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login-wrapper">
+    <h1 class="login-title">Welcome!</h1>
+    <h1 class="login-title">Sistem Poin Siswa</h1>
+
+    <div class="login-form">
+      <h2 class="login-form-title">Please login first!</h2>
+
+      <form action="" class="form-login" method="post">
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input type="text" class="form-control" name="username" id="username" />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="text" class="form-control" name="password" id="password" />
+        </div>
+        <div class="form-group">
+          <button class="btn btn-primary" type="submit" @click.prevent="login()">Login</button>
+        </div>
+      </form>
+
+      <div class="not-registered text-center">
+        <span>Didn't have any account yet? </span>
+        <a href="/register" class="register-link">Register Now</a>
+      </div>
+    </div>
+  </div>
+</template>
