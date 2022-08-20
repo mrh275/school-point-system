e245a77507d1ddc06c7a9f5f5fdb64d98bae42a0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-20 21:36:53 +0700

    added user greetings section

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,10 +1,22 @@
 <template>
-  <h1 class="text-3xl font-bold">Home Page</h1>
+  <div class="user-wrapper flex items-center">
+    <img :src="userImage" alt="User Profile" class="w-14 rounded-full" />
+    <div class="user-detail mx-4">
+      <h4 class="user-name font-semibold text-xl">Halo, John Doe</h4>
+      <span class="text-sm text-gray-500">Administrator</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import userImg from "../../assets/img/user-square.png";
 export default {
   name: "Home",
+  data() {
+    return {
+      userImage: userImg,
+    };
+  },
   mounted() {
     document.title = "Beranda";
   },
