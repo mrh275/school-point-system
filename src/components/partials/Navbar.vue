aedde3873b1aaed5ee9753f91ca2de78ae422372
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-21 21:56:13 +0700

    added nav menu for desktop mode

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -1,13 +1,38 @@
 <template>
-  <nav class="z-50 w-full" :class="[navbarBottom, navbarPosition]">
-    <div class="nav relative py-3 px-4 bg-gray-700 text-white flex justify-between items-center">
+  <nav class="z-50 w-full bg-gray-700 sm:flex sm:justify-between items-center" :class="[navbarBottom, navbarPosition]">
+    <div class="nav relative py-3 px-4 text-white flex justify-between sm:justify-start items-center">
       <img :src="logoImage" class="w-9" alt="Logo Sekolah" />
       <h5 class="px-3 text-lg font-semibold">SMAN 1 RAWAMERTA</h5>
-      <div class="cursor-pointer" @click.prevent="toggleMenu">
+      <div class="cursor-pointer" @click.prevent="toggleMenu" v-if="mobileDevices">
         <i class="bx bxs-grid-alt bx-sm"></i>
       </div>
     </div>
-    <ul class="absolute -z-10 px-4 w-full flex flex-wrap justify-evenly bg-gray-700 text-white text-center transition-all ease-in-out duration-300 rounded-tl-xl rounded-tr-xl" :class="[toggleMenuClass, navMenu]">
+    <ul class="text-white flex justify-evenly px-4" v-if="!mobileDevices">
+      <li class="px-2">
+        <router-link to="/">Beranda</router-link>
+      </li>
+      <li class="px-2 relative" @mouseenter="pelanggaranMenu = 'opacity-100 visible'" @mouseleave="pelanggaranMenu = 'opacity-0 invisible'">
+        <a href="javascript:void(0)">Pelanggaran</a>
+        <ul class="absolute bg-gray-700 px-2 py-2 transition-all ease-in-out w-[120%] duration-300" :class="pelanggaranMenu">
+          <li class="py-1">
+            <router-link to="/about">Daftar Pelanggaran</router-link>
+          </li>
+          <li class="py-1">
+            <router-link to="/">Statistik</router-link>
+          </li>
+          <li class="py-1">
+            <router-link to="/">Tata Tertib</router-link>
+          </li>
+        </ul>
+      </li>
+      <li class="px-2">
+        <router-link to="/">Pengaturan</router-link>
+      </li>
+      <li class="px-2">
+        <router-link to="/">Login</router-link>
+      </li>
+    </ul>
+    <ul class="absolute -z-10 px-4 w-full flex flex-wrap justify-evenly bg-gray-700 text-white text-center transition-all ease-in-out duration-300 rounded-tl-xl rounded-tr-xl" :class="[toggleMenuClass, navMenu]" v-if="mobileDevices">
       <li class="py-5 flex-[33%]">
         <router-link to="/" class="hover:text-[#0099ff]">
           <i class="bx bxs-home bx-sm"></i>
@@ -59,6 +84,8 @@ export default {
       navMenu: "opacity-0",
       navbarPosition: "relative",
       navbarBottom: "bottom-0",
+      mobileDevices: false,
+      pelanggaranMenu: "opacity-0 invisible",
     };
   },
   methods: {
@@ -71,10 +98,18 @@ export default {
         this.navMenu = "opacity-0";
       }
     },
+    hoverToggle() {
+      if (this.pelanggaranMenu == "opacity-0") {
+        this.pelanggaranMenu = "opacity-100";
+      } else {
+        this.pelanggaranMenu = "opacity-0";
+      }
+    },
   },
   mounted() {
-    if (window.innerWidth < 540) {
+    if (window.innerWidth < 640) {
       this.navbarPosition = "fixed";
+      this.mobileDevices = true;
     }
   },
 };
