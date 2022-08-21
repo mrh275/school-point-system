71da4415557923e828dfd5f9c7c0e0994a08e5e7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-08-21 21:56:29 +0700

    added dummy fetch data with axios

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -3,10 +3,26 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "About Me",
+  data() {
+    return {
+      tokenConfig: sessionStorage.getItem("userToken"),
+    };
+  },
   mounted() {
     document.title = " About Me";
+    const config = {
+      headers: { Authorization: `Bearer ${this.tokenConfig}` },
+    };
+    const url = "http://127.0.0.1:8000/api/kelas";
+    axios
+      .get(url, config)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch(console.log);
   },
 };
 </script>
