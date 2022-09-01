b4257f428405327ef4ddd74af938a762f60157fe
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2022-09-01 23:01:26 +0700

    add route for register and home

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../components/pages/Home.vue";
 import About from "../components/pages/About.vue";
 import Login from "../components/auth/Login.vue";
+import Register from "../components/auth/Register.vue";
 
 const routes = [
   {
@@ -13,6 +14,16 @@ const routes = [
     path: "/about",
     name: "About",
     component: About,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/home",
+    name: "Beranda",
+    component: Home,
   },
 ];
 
